d9ab8ba27568fc903fa9ce2768fb2e70fbe8fd5b
1013hs <18437088957@163.com>
2026-08-01 16:57:25 +0800

    Create AppModule.kt

---
+++ app/src/main/java/com/geosurvey/toolbox/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.geosurvey.toolbox.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    
+    @Provides
+    @Singleton
+    fun provideContext(@ApplicationContext context: Context): Context = context
+    
+    // TODO: 阶段4添加Room Database Provider
+    // TODO: 阶段4添加Repository Provider
+}
