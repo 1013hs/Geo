8323fb960a676f411e9653e7110178a3332bdcbd
1013hs <18437088957@163.com>
2026-08-01 16:49:04 +0800

    Create Color.kt

---
+++ app/src/main/java/com/geosurvey/toolbox/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.geosurvey.toolbox.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val TealPrimary = Color(0xFF00897B)
+val TealDark = Color(0xFF004D40)
+val CyanAccent = Color(0xFF00BCD4)
+val CyanLight = Color(0xFF80DEEA)
+
+val GlassBackground = Color(0xE60D1B1E)
+val GlassSurface = Color(0x1AFFFFFF)
+val GlassBorder = Color(0x33FFFFFF)
+
+val TextPrimary = Color(0xFFE0F7FA)
+val TextSecondary = Color(0xFF80DEEA)
+val TextMuted = Color(0xFFB0BEC5)
+
+val StatusGood = Color(0xFF00E676)
+val StatusWarning = Color(0xFFFFD600)
+val StatusError = Color(0xFFFF1744)
